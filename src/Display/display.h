387e6c9208c40bb168f1ccd71495594f1e9278b2
Bernd Giesecke <bernd@giesecke.tk>
2020-03-13 11:02:28 +0800

    Add the sources

---
+++ src/Display/display.h
@@ -0,0 +1,13 @@
+#include <list>
+
+void initDisplay(void);
+void dispWriteHeader(void);
+void dispAddLine(char *line);
+void dispShow(void);
+void dispWrite(String text, int x, int y);
+void dispUpdate(void);
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+#define STATUS_BAR_HEIGHT 12
+#define LINE_HEIGHT 10
+#define MARGIN 3
